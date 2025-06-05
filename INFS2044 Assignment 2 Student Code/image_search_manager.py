e8fe3705ff78c9a98fc0c29117d296cafc2731dd
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-06-05 22:50:28 +0930

    importing all the classes into the search manager file these include image entry, object detection engine, image access, index access and matching engine

---
+++ INFS2044 Assignment 2 Student Code/image_search_manager.py
@@ -0,0 +1,5 @@
+from image_entry import ImageEntry # Import ImageEntry class
+from object_detection_engine import ObjectDetectionEngine # Import ObjectDetectionEngine class
+from image_access import ImageAccess # Import ImageAccess class
+from index_access import IndexAccess # Import IndexAccess class
+from matching_engine import MatchingEngine # Import MatchingEngine class
