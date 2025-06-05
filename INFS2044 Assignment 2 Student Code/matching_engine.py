0274b4791f70ebcf6f61246e281d3554bd899da2
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-06-05 22:39:29 +0930

    adding in an find similar method to find similar vectors and having an list for scores and iterating through the entires to calculate the similarity and store the score and entry

--- INFS2044 Assignment 2 Student Code/matching_engine.py
+++ INFS2044 Assignment 2 Student Code/matching_engine.py
@@ -7,4 +7,9 @@ class MatchingEngine:
     def __int__(self, entries): # Constructor
         self.entries = entries # Store list of image entries 
 
-    
+    def find_similar(self, vector, k): # Find similar vectors
+        scores = [] # List of scores
+        for entry in self.entries:
+            sim = cosine_similarity([vector], [entry.vector])[0][0] # Calculate similarity
+            scores.append((sim, entry)) # Store score and entry
+            
