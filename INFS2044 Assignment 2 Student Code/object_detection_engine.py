2c2ecd63071fe05be321f06f70ef78ffbc700277
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-06-05 22:46:05 +0930

    adding an encode labels method to convert labels to vector then return numpy array of label vector

--- INFS2044 Assignment 2 Student Code/object_detection_engine.py
+++ INFS2044 Assignment 2 Student Code/object_detection_engine.py
@@ -4,3 +4,6 @@ import numpy as np # Import numpy for vector handling
 class ObjectDetectionEngine:
     def detect_objects(self, image): # Detect objects in image
         return list(detect_objects(image)) # Return detected labels as a list
+    
+    def encode_labels(self, labels): # Convert labels to vector
+        return np.array(encode_labels(labels)) # Return numpy array of label vector 
