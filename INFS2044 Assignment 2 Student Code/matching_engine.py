5655dafbdd80fc1a957c6ca3e94dc6817fad1c30
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-06-05 22:37:06 +0930

    adding in the constructor for matching engine and then storing the list of image entries.

--- INFS2044 Assignment 2 Student Code/matching_engine.py
+++ INFS2044 Assignment 2 Student Code/matching_engine.py
@@ -3,3 +3,8 @@ from sklearn.metrics.pairwise import cosine_similarity # Import cosine similarit
 def get_similarity_score(item): # Helper function to get score
     return item[0]
 
+class MatchingEngine:
+    def __int__(self, entries): # Constructor
+        self.entries = entries # Store list of image entries 
+
+    
