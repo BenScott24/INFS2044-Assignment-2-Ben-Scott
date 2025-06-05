4b73bea0384ef82ad6135eddbcb8386ad9f097dd
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-06-05 23:21:18 +0930

    editing the add command by defining it then making it accept a valid path loading the image and detect labels once loaded its processed to image index and is added to index and labels is displayed to the screen

--- INFS2044 Assignment 2 Student Code/image_search.py
+++ INFS2044 Assignment 2 Student Code/image_search.py
@@ -6,15 +6,19 @@ from sklearn.metrics.pairwise import cosine_similarity # Import cosine similarit
 from image_index import ImageIndex # Import image index 
 
 
-@click.group() # Define a group of CLI commands
+@click.group() # Define a group of main commands
 def main():
     pass # Do nothing (placeholder)
 
 
-@main.command()
-@click.argument('image_path', type=click.Path(exists=True, dir_okay=False))
-def add(image_path):
-    pass
+@main.command() # Define 'add' command
+@click.argument('image_path', type=click.Path(exists=True)) # Accepts a valid path
+def add(image_path): # Function to add image
+    image = mpimg.imread(image_path) # Load image
+    labels = detect_objects(image) # Detect labels
+    index = ImageIndex() # Load image index
+    index.add_image(image_path, labels) # Add image to index
+    print("Detected objects " + ",".join(labels)) # Output labels
 
 @main.command()
 @click.option('--all/--some', default=True, show_default=True, help='List images that match all/some query terms')
