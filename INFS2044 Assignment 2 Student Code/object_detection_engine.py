cb529774e943ee784c8925fe738ee1cb65d89408
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-06-05 22:44:54 +0930

    creating an detect objects method for detecting objects in the image and then returning the detected labels as a list

--- INFS2044 Assignment 2 Student Code/object_detection_engine.py
+++ INFS2044 Assignment 2 Student Code/object_detection_engine.py
@@ -1,2 +1,6 @@
 from object_detector import detect_objects, encode_labels # Import from detector
 import numpy as np # Import numpy for vector handling
+
+class ObjectDetectionEngine:
+    def detect_objects(self, image): # Detect objects in image
+        return list(detect_objects(image)) # Return detected labels as a list
