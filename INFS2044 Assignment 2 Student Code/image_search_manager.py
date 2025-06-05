0fd58a5f5d32edc574e66f3be311b752ff4660eb
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-06-05 22:52:41 +0930

    creating an constructor for image search manager then assigning the following objects object detection engine, image access object and index access object

--- INFS2044 Assignment 2 Student Code/image_search_manager.py
+++ INFS2044 Assignment 2 Student Code/image_search_manager.py
@@ -3,3 +3,9 @@ from object_detection_engine import ObjectDetectionEngine # Import ObjectDetecti
 from image_access import ImageAccess # Import ImageAccess class
 from index_access import IndexAccess # Import IndexAccess class
 from matching_engine import MatchingEngine # Import MatchingEngine class
+
+class ImageSearchManager:
+    def __init__(self): # Constructor for ImageSearchManager
+        self.detector = ObjectDetectionEngine() # Create object detection engine
+        self.access = ImageAccess() # Create image access object
+        self.index = IndexAccess() # Create index access object
