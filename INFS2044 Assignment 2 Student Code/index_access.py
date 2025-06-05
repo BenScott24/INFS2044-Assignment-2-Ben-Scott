8c24b5ab3c5f195c95966e70e988cd19011e01d6
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-06-05 22:14:40 +0930

    adding an add entry method which adds an image entry, checks for duplicates if so it skips if duplicate otherwise it adds an new entry

--- INFS2044 Assignment 2 Student Code/index_access.py
+++ INFS2044 Assignment 2 Student Code/index_access.py
@@ -1,4 +1,11 @@
 class IndexAccess:
     def __init__(self): # Constructor
         self.entries = [] # List to hold image entries
+
+    def add_entry(self, entry): # Add image entry
+        for index in self.entries: # Check for duplicates
+            if index.path == entry.path:
+                return # Skip if duplicate
+        self.entries.append(entry) # Add new entry
+
     
